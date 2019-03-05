00cc8aed9f3b6a4a9dda0b67b7d5175787f549a7
Digvijay <digv22@gmail.com>
2019-03-05 20:29:56 +0530

    update

---
+++ device/index.js
@@ -0,0 +1,37 @@
+var Gpio = require('onoff').Gpio; 
+
+var http = require("http");
+
+var relay1GPIO = new Gpio(2, 'out');
+var relay2GPIO = new Gpio(3, 'out');
+
+var relay3GPIO = new Gpio(4, 'out');
+var relay4GPIO = new Gpio(17, 'out');
+
+
+relay1GPIO.writeSync(0);
+relay2GPIO.writeSync(0);
+relay3GPIO.writeSync(0);
+relay4GPIO.writeSync(0);
+
+
+let updateInterval = setInterval(() => {
+
+    http.get({ host: '13.234.106.208', port: 3000, path: '/state', method: 'GET' }, function (res) {
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            console.log(chunk);
+            
+            let data = JSON.parse(chunk); 
+
+            relay1GPIO.writeSync(data.relay1);
+            relay2GPIO.writeSync(data.relay2);
+            relay3GPIO.writeSync(data.relay3);
+            relay4GPIO.writeSync(data.relay4);
+        });
+
+
+    });
+
+
+}, 5000);
